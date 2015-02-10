3d43a9b55f983ea0967f931ecc28f31dd6b7e93d
Alisson Elias <alissonelias77@gmail.com>
2015-02-09 21:33:10 -0300

    iniciação da pagina login

--- index.php
+++ index.php
@@ -119,6 +119,30 @@
                 </div>
             </div>
         </div>
+        <!--<alisssoonnnnnnnnnnnnnnnnn>-->
+        <form class="form-horizontal">
+            <img src="img/Capa.jpg" alt="capa" />
+            <div class="control-group">
+                <label class="control-label" for="inputEmail">Email</label>
+                <div class="controls">
+                    <input type="text" id="inputEmail" placeholder="Email">
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="inputPassword">Senha</label>
+                <div class="controls">
+                    <input type="password" id="inputPassword" placeholder="Senha">
+                </div>
+            </div>
+            <div class="control-group">
+                <div class="controls">
+                    <label class="checkbox">
+                        <input type="checkbox"> Esqueceu sua senha?
+                    </label>
+                    <button type="submit" class="btn">Entrar</button>
+                </div>
+            </div>
+        </form>
     </div>
 
 
