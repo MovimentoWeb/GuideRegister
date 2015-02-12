e8c5d742885c67407dcdc9b9186edfca250b8fe3
Reginaldo Lima <reginaldo963@gmail.com>
2015-02-12 00:07:04 -0300

    Criação do footer desktop

---
+++ paginas/includes/header.php
@@ -0,0 +1,104 @@
+<header id="header-menu" class="navbar navbar-fixed-top navbar-inverse">
+
+    <div class="navbar-inner">
+        <div class="container">
+            <!-- .btn-navbar é usado como alternador para conteúdo de barra de navegação colapsável -->
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+
+            <!-- Tenha certeza de deixar a marca se você quer que ela seja mostrada -->
+            <a class="brand" href="#"><span class="fa-home3"></span> Guide Register</a>
+
+            <!-- Tudo que você queira escondido em 940px ou menos, coloque aqui -->
+            <div class="nav-collapse">
+                <!-- .nav, .navbar-search, .navbar-form, etc -->
+                <ul class="nav">
+                    <div class="btn-group">
+                        <a class="btn dropdown-toggle btn-inverse" data-toggle="dropdown" href="#">
+                            <i class="fa-user"></i> Aluno
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <!-- Links de menu dropdown -->
+
+                            <li ><a href="#"><span class="fa-user-add"></span> Cadastrar Aluno</a> </li>
+                            <li class="dropdown-submenu">
+                                <a tabindex="-1" href="#"><i class="fa-search"></i> Pesquisar</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="">Pesquisa do Aluno</a></li>
+                                    <li><a href="">Ata de turmas</a></li>
+                                    <li><a href="">Pesquisa de disciplina</a></li>
+
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>  
+                </ul> 
+
+                <ul class="nav">
+                    <div class="btn-group">
+                        <a class="btn dropdown-toggle btn-inverse" data-toggle="dropdown" href="#">
+                            <span class="fa-users4"></span> Professores
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <!-- Links de menu dropdown -->
+
+                            <li ><a href="#"><span class="fa-user-add"></span> Cadastrar Professor</a> </li>
+                            <li class="dropdown-submenu">
+                                <a tabindex="-1" href="#"><span class="fa-list2"></span> Listar</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="">Listas</a></li>
+                                    <li><a href="">Pesquisa do Professor</a></li>
+                                    <li><a href="">Resultados</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>  
+                </ul>  
+                <ul class="nav">
+                    <div class="btn-group">
+                        <a class="btn dropdown-toggle btn-inverse" data-toggle="dropdown" href="#">
+                            <i class="fa-folder-open"></i> Turmas
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <!-- Links de menu dropdown -->
+
+                            <li ><a href="#"> <i class="fa-plus"></i> Cadastrar</a> </li>
+                            <li class="dropdown-submenu">
+                                <a tabindex="-1" href="#"><i class="fa-list-alt"></i> Listar</a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="">Listas</a></li>
+                                    <li><a href="">Pesquisa da Turma</a></li>
+                                    <li><a href="">Resultados</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>  
+                </ul> 
+                <ul class="nav">
+                    <div class="btn-group">
+                        <a class="btn dropdown-toggle btn-inverse" data-toggle="dropdown" href="#">
+                            <i class="fa-file-powerpoint icon-white"></i> Relatórios
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <!-- Links de menu dropdown -->
+
+                            <li ><a href="?selecionar=relatorios">Pesquisar por aluno</a> </li>
+                            <li ><a href="#">Ata por turmas</a> </li>
+                            <li ><a href="#">Pesquisar por disciplinas</a> </li>
+                        </ul>
+                    </div>  
+                </ul>  
+                <ul class="nav">
+                    <a class="btn btn-inverse" href=""><span class="fa-warning2"></span> Aviso</a> 
+                </ul>  
+            </div>
+        </div>
+    </div>
+</header>
