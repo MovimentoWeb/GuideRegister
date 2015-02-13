c11fcf6fa3a618f1d0c26dc0e58b5e5c22b0d0dd
Alexsander André <Alexsander_andre10@hotmail.com>
2015-02-12 21:09:06 -0300

    Pagina de professores (PRONTA) AHHHHHHHHHH !!!!!!!!!!!!!!!!!!

--- telas/professores/professores.php
+++ telas/professores/professores.php
@@ -1,15 +1,40 @@
-
-        <div class="container">
-            <div class="professores well">
-                <form class="form-horizontal">
-                    <legend><span class="fa-user-add"></span> Cadastro de professores</legend>
-                    <input type="text" placeholder="Nome"  />
-                    <input type="text" placeholder="Data de Nascimento" />
-                    <input type="text" placeholder="RG" />
-                    <input type="text" placeholder="CPF" />
-                    <input type="text" placeholder="Data de Admissão" />
-                    <select name="Turno" id="" ></select>
-                    <input class="btn btn-large btn-block" type="submit" value="Enviar" />
-                </form>
-            </div>
-        </div>
+<div class="container">
+    <div class="professores well">
+        <form class="form-horizontal">
+            <legend><span class="fa-user-add"></span> Cadastro de professores</legend>
+            <span class="help-block">Preencha corretamente os acampos abaixo com os  dados do professor</span>
+            <ul>
+                <li>
+                    <label for="">Nome do professor:</label>
+                    <input class="input-block-level" type="text" placeholder="Nome"  />
+                </li>
+                <li>
+                    <label for="">Data de Nascimento:</label>
+                    <input class="input-block-level" type="text" placeholder="Data de nascimento"  />
+                </li>
+                <li>
+                    <label for="">Rg:</label>
+                    <input class="input-block-level" type="text" placeholder="RG"  />
+                </li>
+                <li>
+                    <label for="">CPF:</label>
+                    <input class="input-block-level" type="text" placeholder="CPF"  />
+                </li>
+                <li>
+                    <label for="">Data de admissão:</label>
+                    <input class="input-block-level" type="text" placeholder="Data de admissão"  />
+                </li>
+                <li>
+                    <label>Turno</label>
+                    <select>
+                        <option value="">Selecione um turno</option>
+                        <option value="">Manhã</option>
+                        <option value="">Tarde</option>
+                        <option value="">Noite</option>
+                    </select>
+                </li>               
+            </ul>
+            <input class="btn btn-primary" type="submit" value="Enviar" />
+        </form>
+    </div>
+</div>
