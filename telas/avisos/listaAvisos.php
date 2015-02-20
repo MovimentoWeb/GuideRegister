f4f21e1199d9cdccd54470d80bbb7b24bfa756da
Joao VIctor <jooao.victoor8@gmail.com>
2015-02-19 21:20:23 -0300

    Marcelo/Joao Commit

--- telas/avisos/listaAvisos.php
+++ telas/avisos/listaAvisos.php
@@ -1,14 +1,40 @@
-<div class="meioTurma ">
-    <form class="well">
+<div id="ListadeAvisos" class="meioTurma well">
+    <form class="">
         <legend><span class="fa-notification"></span> Avisos</legend>
-    <span class="help-block">Preencha corretamente os acampos abaixo </span>
-    
-    <input type="button" class=" btn btn-inverse span3" value="Professor"  />
-    <input type="button" class=" btn btn-inverse span3" value="Justificativa Aluno"  /><br />
-    <textarea placeholder="Digite aqui seu aviso" rows="3" class="span6"></textarea><br />
-    <input type="button" class=" btn btn-success span3" value="Aceitar"  />
-    <input type="button" class=" btn btn-danger span3" value="Rejeitar"  /><br />
-    
-    <input type="button" class=" btn btn-primary span3" value="Enviar"  />
-</form>
+        <span class="help-block">Preencha corretamente os acampos abaixo </span>
+
+
+        <div class="row-fluid">
+            <div class="span6"> 
+                <select>
+                    <option>Professor</option>
+                    <option>Professor2</option>
+                    <option>Professor3</option>
+                    <option>Professor4</option>
+                    <option>Professor5</option>
+                </select>
+            </div>
+            <div class="span6">   
+                <select class>
+                    <option>Just.Aluno</option>
+                    <option>Just.Aluno2</option>
+                    <option>Just.Aluno3</option>
+                    <option>Just.Aluno4</option>
+                    <option>Just.Aluno5</option>
+                </select>
+            </div>
+        </div>
+
+
+
+
+
+        <textarea placeholder="Digite aqui seu aviso" rows="5" class="input-block-level"></textarea><br />
+
+
+        <button class="btn btn-success">Aceitar</button>
+        <button class="btn btn-danger">Rejeitar</button>
+        <button class="btn btn-primary">Enviar</button>
+
+    </form>
 </div>
