808d5a3020c35570ef64f3ac4e573a8455879b71
PC09 <PC09@PC09-HP.mshome.net>
2015-02-23 20:11:54 -0300

    khbkkbhkbhkbjkbh

--- telas/relatorios/relatorios.php
+++ telas/relatorios/relatorios.php
@@ -4,20 +4,31 @@
 <div class="well">
     
     <div class="container relatorios"> 
-        <legend><span class="fa-user-add"></span> Pesquisar por Aluno</legend>
-        <form class="form-search">
-            <div class="input-append">
-                <input type="text" class="span2 search-query">
-                <button type="submit" class="btn">Pesquisar</button>
-            </div>
-        </form>  
+        <legend><span class="fa-search3"></span>  Pesquisar Alunos</legend>
+        
+       <form class="form-search"> 
+           
+            <span class="help-block">Digite um nome de aluno para pesquisa</span>
+            
+            <ul>
+                <li>
+                    <div class="input-append">
+                        <input type="text" class="search-query input-block-level">
+                        <!--                        <button type="submit" class="btn btn-primary">Busca</button>-->
+                        <a href="?pg=dadosAluno" class="btn btn-primary"> Buscar</a>
+                    </div>
+                </li>
+            </ul> 
+            
+       </form>  
+        
     </div>
 
     <div class=" container tabelas">
         <table class="table table-striped">
             <thead>
                 <tr>
-                    <th>#</th>
+                    <th>Matricula</th>
                     <th>Nome</th>
                     <th>Sobrenome</th>
                 </tr>
@@ -26,11 +37,22 @@
             <tbody>
 
                 <tr>
-                    <td>1</td>
-                    <td>Funéria</td>
-                    <td></td>
+                    <td>00000</td>
+                    <td>Alisson</td>
+                    <td>Elias</td>
                 </tr>
-
+                
+                <tr>
+                    <td>00001</td>
+                    <td>Felipe</td>
+                    <td>Reis</td>
+                </tr>
+                
+                <tr>
+                    <td>00002</td>
+                    <td>Julia</td>
+                    <td>França</td>
+                </tr>
             </tbody>
 
         </table>
