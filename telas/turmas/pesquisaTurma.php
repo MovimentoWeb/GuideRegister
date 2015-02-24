f7f92bd22bc882bd287c99a1bca7e64f45730907
Joao VIctor <jooao.victoor8@gmail.com>
2015-02-24 20:47:42 -0300

    Commit Joaozito

--- telas/turmas/pesquisaTurma.php
+++ telas/turmas/pesquisaTurma.php
@@ -4,19 +4,15 @@ include_once 'telas/turmas/validacaoTurmas.php';
 <div class="PesquisarAluno">
     <div class="well">  
         <form class="form-search">
-            <legend><span class="fa-search3"></span>  Pesquisar Alunos</legend>
-            <span class="help-block">Digite um nome de aluno para pesquisa</span>
+            <legend><span class="fa-search3"></span>  Pesquisar Turmas</legend>
+            <span class="help-block">Digite um nome da turma para pesquisa</span>
 
             <ul>
                 <li>
                     <div class="input-append">
                         <input type="text" class="search-query input-block-level">
                         <!--                        <button type="submit" class="btn btn-primary">Busca</button>-->
-                        <?php if ($temErros && isset($errosValidacao['pesq'])) : ?>
-                            <span class="erro">
-                                <?php echo $errosValidacao['pesq']; ?>
-                            </span>
-                        <?php endif; ?>
+
                         <a class="btn btn-primary" name="pesq" value="<?php echo $projeto['pesq']; ?>"> Buscar</a>
                     </div>
                 </li>
@@ -24,3 +20,8 @@ include_once 'telas/turmas/validacaoTurmas.php';
         </form>
     </div>
 </div>
+<?php if ($temErros && isset($errosValidacao['pesq'])) : ?>
+    <span class="erro">
+        <?php echo $errosValidacao['pesq']; ?>
+    </span>
+<?php endif; ?>
