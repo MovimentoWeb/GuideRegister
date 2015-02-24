3ccb1730b39aa5e80e9c89849741f67aa27c0153
Alisson Elias <alissonelias77@gmail.com>
2015-02-24 20:31:48 -0300

    validação da pagina login(quase pronta)....

--- telas/login/validacaologin.php
+++ telas/login/validacaologin.php
@@ -6,6 +6,8 @@ $temErros = false;
 $errosValidacao = array();
 
 $exibirTabela = TRUE;
+
+//Validação Formulário de login
 if (temPost()) {
     $verificacao = array();
 
@@ -15,7 +17,7 @@ if (temPost()) {
     } else {
         $temErros = true;
         $errosValidacao['email'] = ''
-        . '<div class="alert alert-error">'
+                . '<div class="alert alert-error">'
                 . '<button type="button" class="close" data-dismiss="alert">×</button>'
                 . '<h4>E-mail inválido!</h4>'
                 . 'Digite corretamente o E-mail'
@@ -23,51 +25,83 @@ if (temPost()) {
     }
 
 //Validação da SENHA  
-    if (isset($_POST['senha']) && strlen($_POST['senha']) >=8) {
+    if (isset($_POST['senha']) && strlen($_POST['senha']) >= 8) {
         $verificacao['senha'] = $_POST['senha'];
     } else {
         $temErros = true;
         $errosValidacao['senha'] = ''
-            . '<div class="alert alert-error">'
+                . '<div class="alert alert-error">'
                 . '<button type="button" class="close" data-dismiss="alert">×</button>'
                 . '<h4>Senha inválida!</h4>'
                 . 'Digite corretamente a Senha'
-                . '</div>';;
-    }
-    //Validação da esqueciMinhaSenha  
-    if (isset($_POST['esqueciMinhaSenha']) && strlen($_POST['esqueciMinhaSenha']) >=8) {
-        $verificacao['esqueciMinhaSenha'] = $_POST['esqueciMinhaSenha'];
-    } else {
-        $temErros = true;
-        $errosValidacao['esqueciMinhaSenha'] = ''
-            . '<div class="alert alert-error">'
-                . '<button type="button" class="close" data-dismiss="alert">×</button>'
-                . '<h4>Senha inválida!</h4>'
-                . 'Digite corretamente a Senha'
-                . '</div>';;
-    }
-    //Validação da NOVASENHA  
-    if (isset($_POST['novasenha']) && strlen($_POST['novasenha']) >=10) {
-        $verificacao['novasenha'] = $_POST['novasenha'];
-    } else {
-        $temErros = true;
-        $errosValidacao['novasenha'] = ''
-            . '<div class="alert alert-error">'
-                . '<button type="button" class="close" data-dismiss="alert">×</button>'
-                . '<h4>Nova Senha inválida!</h4>'
-                . 'Digite corretamente a NoSva Senha'
-                . '</div>';;
+                . '</div>';
     }
     if (!$temErros) {
         header('Location: index.php?pg=login&opcao=loginInicial');
         die();
     }
+//Validação formulário - ESQUECI MINHA SENHA
+    if (temPost() && isset($_POST['email'])) {
+        $verificacao = array();
+        if (isset($_POST['email']) && strlen($_POST['email']) > 3) {
+            $verificacao['email'] = $_POST['email'];
+
+            
+        } else {
+            $temErros = true;
+            $errosValidacao['email'] = ''
+                    . '<div class="alert alert-error">'
+                    . '<button type="button" class="close" data-dismiss="alert">×</button>'
+                    . '<h4>E-mail inválido!</h4>'
+                    . 'Digite corretamente o E-mail'
+                    . '</div>';
+        }
+    }
+
+//Validação da NOVA SENHA  
+    if (temPost() && isset($_POST['novasenha'])) {
+        $verificacao = array();
+
+        if (isset($_POST['novasenha']) && strlen($_POST['novasenha']) >= 10) {
+            $verificacao['novasenha'] = $_POST['novasenha'];
+            
+            //tem que está dentro da função recuperar senha
+        echo '<div class="alert alert-success">'
+        . '<button type="button" class="close" data-dismiss="alert">×</button>'
+        . '<h4>Email enviado com sucesso!</h4>'
+        . 'Verifique sua caixa de E-mail para ver sua nova senha.'
+        . '</div>';
+        } else {
+            $temErros = true;
+            $errosValidacao['novasenha'] = ''
+                    . '<div class="alert alert-error">'
+                    . '<button type="button" class="close" data-dismiss="alert">×</button>'
+                    . '<h4>Nova Senha inválida!</h4>'
+                    . 'Digite corretamente a Nova Senha'
+                    . '</div>';
+        }
+    }
+//Validação da CONFIRMARSENHA  
+    if (temPost() && isset($_POST['confirmasenha'])) {
+        $verificacao = array();
+        if (isset($_POST['confirmasenha']) && strlen($_POST['confirmasenha']) >= 10) {
+            $verificacao['confirmasenha'] = $_POST['confirmasenha'];
+        } else {
+            $temErros = true;
+            $errosValidacao['confirmasenha'] = ''
+                    . '<div class="alert alert-error">'
+                    . '<button type="button" class="close" data-dismiss="alert">×</button>'
+                    . '<h4>Nova Senha inválida!</h4>'
+                    . 'Digite corretamente a Nova Senha'
+                    . '</div>';
+        }
+    }
+
+    $verificacao = array(
+        'id' => 0,
+        'email' => (isset($_POST['email'])) ? $_POST['email'] : '',
+        'senha' => (isset($_POST['senha'])) ? $_POST['senha'] : '',
+        'novasenha' => (isset($_POST['novasenha'])) ? $_POST['novasenha'] : '',
+        'confirmasenha' => (isset($_POST['confirmasenha'])) ? $_POST['confirmasenha'] : ''
+    );
 }
-$verificacao = array(
-    'id' => 0,
-    'email' => (isset($_POST['email'])) ?
-            $_POST['email'] : '',
-    'senha' => (isset($_POST['senha'])) ? $_POST['senha'] : '',
-    'novasenha' => (isset($_POST['novasenha'])) ? $_POST['senha'] : ''
-    
-);
