1ec3d02167c0c30772c7281553c5a03a9fe4ed46
Reginaldo Lima <reginaldo963@gmail.com>
2015-03-01 13:28:37 -0300

    Conserto Menu Mobile(ok)

--- telas/includes/header.php
+++ telas/includes/header.php
@@ -9,11 +9,13 @@
             </a>
 
             <!-- Tenha certeza de deixar a marca se você quer que ela seja mostrada -->
-            <a class="brand" href="index.php"><img src="img/GuideRegister-Icon.ico" height="30px" alt="Página inicial" title="Página inicial"/> <em class="visible-desktop">Guide Register</em></a>
+            <a class="brand" href="index.php">
+                <img src="img/GuideRegister-Icon.ico"  alt="Página inicial" title="Página inicial"/> 
+                Guide Register
+            </a>
 
             <ul class="nav">
                 <li><a class="brand"  href="?pg=avisosEjustificativas&opcao=meusAvisos"><span class="fa-bell"></span></a></li>
-
                 <li><a class="brand"  href="?pg=creditos"><span class="fa-coin"></span></a></li>
             </ul>
             <!-- Tudo que você queira escondido em 940px ou menos, coloque aqui -->
@@ -21,7 +23,7 @@
                 <!-- .nav, .navbar-search, .navbar-form, etc -->
                 <ul class="nav">
 
-                   <li class="dropdown">
+                    <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                             Aluno
                             <b class="caret"></b>
@@ -32,27 +34,12 @@
                             <li><a href="?pg=aluno&opcao=pesquisar"><span class="fa-search3"></span> Pesquisar</a> </li>
                         </ul>
                     </li>
-
                     <li class="dropdown">
-                        <a class="btn dropdown-toggle btn-inverse" data-toggle="dropdown" href="#">
-                            <i class="fa-user"></i> Aluno
-                            <span class="caret"></span>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            Professores
+                            <b class="caret"></b>
                         </a>
                         <ul class="dropdown-menu">
-                            <!-- Links de menu dropdown -->
-                            <li><a href="?pg=aluno&opcao=AtaFrequencia"><span class="fa-list3"></span> Fazer frequência de aluno</a></li>
-                            <li><a href="?pg=aluno&opcao=cadastrar"><span class="fa-user-add"></span> Cadastrar</a> </li>
-                            <li><a href="?pg=aluno&opcao=pesquisar"><span class="fa-search3"></span> Pesquisar</a> </li>
-                        </ul>
-                    </li>  
-                    <li>
-                        <a class="btn dropdown-toggle btn-inverse" data-toggle="dropdown" href="#">
-                            <span class="fa-users4"></span> Professores
-                            <span class="caret"></span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <!-- Links de menu dropdown -->
-
                             <li ><a href="?pg=professores&opcao=cadastrar"><span class="fa-user-add"></span> Cadastrar</a> </li>
                             <li><a href="?pg=professores&opcao=pesquisar"><span class="fa-search3"></span> Pesquisar</a></li>
                             <li class="dropdown-submenu">
@@ -64,46 +51,37 @@
                                 </ul>
                             </li>
                         </ul>
-                    </li>  
-                    <li>
-                        <a class="btn dropdown-toggle btn-inverse" data-toggle="dropdown" href="#">
-                            <i class="fa-folder-open"></i> Turmas
-                            <span class="caret"></span>
+                    </li> 
+
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            Turmas
+                            <b class="caret"></b>
                         </a>
                         <ul class="dropdown-menu">
-                            <!-- Links de menu dropdown -->
-
                             <li ><a href="?pg=turmas&opcao=cadastrar"> <i class="fa-plus"></i> Cadastrar Turmas</a></li>
                             <li ><a href="?pg=turmas&opcao=Disciplina"> <i class="fa-plus"></i> Cadastrar Disciplina</a></li>
                             <li><a href="?pg=turmas&opcao=pesquisar"><span class="fa-search3"></span> Pesquisa</a></li>
                         </ul>
-                    </li>  
+                    </li>
 
-                    <!------------------------------- Relatorios - Taynara ------------------------------------> 
-                    <li>
-                        <a class="btn dropdown-toggle btn-inverse" data-toggle="dropdown" href="#">
-                            <i class="fa-file-powerpoint"></i> Relatórios
-                            <span class="caret"></span>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            Relatórios
+                            <b class="caret"></b>
                         </a>
                         <ul class="dropdown-menu">
-                            <!-- Links de menu dropdown -->
-                            <li ><a href="?pg=relatorios&opcao=atasPorTurmas"><span class="fa-list" ></span> Listar turma por PA</a> </li>
-                            <li ><a href="?pg=relatorios&opcao=listagemTurmas"><span class="fa-circle-full" ></span> Todas as turmas</a> </li>
-                            <li ><a href="?pg=relatorios&opcao=pesquisarDisciplinas"><span class="fa-search3"></span> Pesquisar disciplinas</a> </li>
+                            <li><a href="?pg=relatorios&opcao=atasPorTurmas"><span class="fa-list" ></span> Listar turma por PA</a> </li>
+                            <li><a href="?pg=relatorios&opcao=listagemTurmas"><span class="fa-circle-full" ></span> Todas as turmas</a> </li>
+                            <li><a href="?pg=relatorios&opcao=pesquisarDisciplinas"><span class="fa-search3"></span> Pesquisar disciplinas</a> </li>
                         </ul>
-                    </li>  
-
-                    <!-------------------------------Fim Relatorios - Taynara ------------------------------------> 
-
-
-                    <li>
-                        <a class="btn dropdown-toggle btn-inverse" data-toggle="dropdown" href="#">
-                            <i class="fa-help"></i> Avisos e Justificativas 
-                            <span class="caret"></span>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            Avisos e Justificativas 
+                            <b class="caret"></b>
                         </a>
                         <ul class="dropdown-menu">
-                            <!-- Links de menu dropdown -->
-
                             <li><a href="?pg=avisosEjustificativas&opcao=novoAviso"> <span class="fa-plus"></span> Novo Aviso</a></li>
                             <li><a href="?pg=avisosEjustificativas&opcao=meusAvisos"> <span class="fa-screen3"></span> Ver meus avisos</a></li>
                             <li class="divider"></li>
